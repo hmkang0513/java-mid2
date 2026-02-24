7d1dcf9c485e1bf1896e42216f4284c81b7572b1
kanghm <kanghm0513@khu.ac.kr>
2026-02-24 15:19:04 +0900

    12. 타입 매개변수 제한2 - 다형성 시도

--- src/generic/ex3/AnimalHospitalMainV0.java
+++ src/generic/ex3/AnimalHospitalMainV0.java
@@ -3,7 +3,7 @@ package generic.ex3;
 import generic.animal.Cat;
 import generic.animal.Dog;
 
-public class AnimalHospital {
+public class AnimalHospitalMainV0 {
     public static void main(String[] args) {
         DogHospital dogHospital = new DogHospital();
         CatHospital catHospital = new CatHospital();
