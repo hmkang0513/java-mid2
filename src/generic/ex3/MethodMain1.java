4715685e817f362e8939643910e08f8466799ebf
kanghm <kanghm0513@khu.ac.kr>
2026-02-24 16:26:02 +0900

    15. 제네릭 메서드

--- src/generic/ex3/MethodMain1.java
+++ src/generic/ex3/MethodMain1.java
@@ -9,8 +9,13 @@ public class MethodMain1 {
         // 타입 인자 (Type Argument) 명시적 전달
         System.out.println("명시적 타입 전달");
         Integer result = GenericMethod.<Integer>genericMethod(i);
-//        System.out.println("result = " + result);
         Integer integerValue = GenericMethod.<Integer>numberMethod(10);
         Double doubleValue = GenericMethod.<Double>numberMethod(20.0);
+
+        System.out.println("타입 추론");
+        Integer result2 = GenericMethod.genericMethod(i);
+        Integer integerValue2 = GenericMethod.numberMethod(10);
+        Double doubleValue2 = GenericMethod.numberMethod(20.0);
+
     }
 }
