b762971e9ab23067a11377d4838a15035bc38d61
KangHyemi <kanghm@gmail.com>
2026-02-25 16:23:23 +0900

    19. 타입 이레이저

---
+++ src/generic/test/ex5/EraserBox.java
@@ -0,0 +1,12 @@
+package generic.test.ex5;
+
+public class EraserBox {
+    public boolean instanceCheck(Object param){
+//        return param instanceof T;
+        return false;
+    }
+
+    public void create(){
+//        return new T();
+    }
+}
