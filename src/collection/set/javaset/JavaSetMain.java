8b45a69f2fbd6a5f992488ad8c9608dc1af6afeb
KANG HYEMI <kanghm0513@gmail.com>
2026-03-09 16:24:08 +0900

    66. 자바가 제공하는 Set3 - 예제

--- src/collection/set/javaset/JavaSetMain.java
+++ src/collection/set/javaset/JavaSetMain.java
@@ -22,7 +22,7 @@ public class JavaSetMain {
 
         Iterator<String> iterator = set.iterator();
         while(iterator.hasNext()){
-            System.out.println(iterator.next() + " ");
+            System.out.print(iterator.next() + " ");
         }
         System.out.println();
     }
